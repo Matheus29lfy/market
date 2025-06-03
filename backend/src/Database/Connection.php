995e3b45d5159d3465de48a8dfcb3bcd44e7bcfd
Matheus29lfy <matheus29lfy@gmail.com>
2025-06-03 12:15:29 -0300

    Fix:Validaçao removido var_dump

--- backend/src/Database/Connection.php
+++ backend/src/Database/Connection.php
@@ -15,13 +15,13 @@ class Connection {
             $pass = getenv('DB_PASSWORD');
             $port = getenv('DB_PORT');
 
-            var_dump([
-  'host' => getenv('DB_HOST'),
-  'port' => getenv('DB_PORT'),
-  'db'   => getenv('DB_DATABASE'),
-  'user' => getenv('DB_USERNAME'),
-  'pass' => getenv('DB_PASSWORD'),
-]);
+//             var_dump([
+//   'host' => getenv('DB_HOST'),
+//   'port' => getenv('DB_PORT'),
+//   'db'   => getenv('DB_DATABASE'),
+//   'user' => getenv('DB_USERNAME'),
+//   'pass' => getenv('DB_PASSWORD'),
+// ]);
 
           $dsn = "pgsql:host={$host};port={$port};dbname={$db}";
           $this->pdo = new PDO($dsn,$user,$pass);
